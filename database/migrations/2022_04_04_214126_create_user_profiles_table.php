ea712acccc3868012870fe940420e7668d631cfe
AhdKarman <karahdman1@gmail.com>
2022-04-05 01:03:00 +0300

    Migrate Tables in DB

--- database/migrations/2022_04_04_214126_create_user_profiles_table.php
+++ database/migrations/2022_04_04_214126_create_user_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Jobs extends Migration
+class CreateUserProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class Jobs extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class Jobs extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('user_profiles');
     }
 }
