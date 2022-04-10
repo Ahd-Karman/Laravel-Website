901b077715f263d4802a7a9f2142ced0bd2fc066
AhdKarman <karahdman1@gmail.com>
2022-04-10 16:10:59 +0300

    Login

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
 
     public function login(Request $request){
       Validator::validate($request->all(),[
-        'user_email'=>['email','required','min:3','unique:users, email'],
+        'user_email'=>['email','required'],
         'user_pass'=>['required','min:5']
        ],[
            'user_email.required' => 'Error! Can not be null' ,
@@ -32,7 +32,7 @@ class AuthController extends Controller
        $u->name = $request->input('user_email');
        $u->save();*/
 
-       if(Auth::attempt(['email'=>$request->email_username,'password'=>$request->user_pass])){
+       if(Auth::attempt(['email'=>$request->user_email,'password'=>$request->user_pass])){
 
             
         if(Auth::user()->hasRole('admin'))
@@ -42,24 +42,27 @@ class AuthController extends Controller
 
     
     }
+    else {
+        return redirect()->route('login')->with(['message'=>'incorerct username or password or your account is not active ']);
+    }
     }
 
     public function signUp(Request $request){
         Validator::validate($request->all(),[
-            'full_name'=>['required','min:5'],
+            'full_name'=>['required','min:3'],
             'user_email'=>['required','email','unique:users,email'],
             'user_pass'=>['required','min:5'],
             'confirm_pass'=>['same:user_pass']
 
 
         ],[
-            'full_name.required'=>'password is required',
-            'full_name.min'=>'password should not be more than 5',
+            'full_name.required'=>'FullName is required',
+            'full_name.min'=>'FullName should not be less than 3',
             'user_email.unique'=>'there is an email in the table',
             'user_email.required'=>'this field is required',
             'user_email.email'=>'incorrect email format',
             'user_pass.required'=>'password is required',
-            'user_pass.min'=>'password should not be more than 5',
+            'user_pass.min'=>'password should not be less than 5',
             'confirm_pass.same'=>'password dont match',
         ]);
 
@@ -67,9 +70,35 @@ class AuthController extends Controller
         $u->name=$request->full_name;
         $u->password=Hash::make($request->user_pass);
         $u->email=$request->user_email;
-        if($u->save())
-        return redirect()->route('home')
-        ->with(['success'=>'user created successful']);
+        if($u->save()){
+            $u->attachRole('admin');
+            return redirect()->route('home')
+            ->with(['success'=>'user created successful']);
+        }
+
+      
         return back()->with(['error'=>'can not create user']);
     }
+
+
+    public function editUser(){
+        $u=User::find(5);
+        if($u->hasRole('admin'))
+        {
+            
+        }
+        else {
+            
+        }
+    }
+    public function resetPassword(){
+
+    }
+    public function logout(){
+
+        Auth::logout();
+        return redirect()->route('login');
+
+    }
+
 }
